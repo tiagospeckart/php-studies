1663148b27d4063f66b9fe0a7ae330c09ace1b92
Tiago Martins Speckart <tiagospeckart@gmail.com>
2024-11-11 13:42:37 -0300

    initial commit

---
+++ php-docker/src/public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * PHP Info display script
+ *
+ * This file contains code to display PHP configuration information
+ *
+ * PHP version 8.x
+ *
+ * @category System
+ * @package  Core
+ * @author   Your Name <your.email@example.com>
+ * @license  http://www.php.net/license/3_01.txt PHP License 3.01
+ * @version  GIT: $Id$
+ * @link     http://yourwebsite.com
+ */
+
+declare(strict_types = 1);
+
+phpinfo();
